8e0d4eb2465ddd5736aa518ba9e0d1b81a133b42
System <Mr.NN91@gmail.com>
2017-02-21 14:29:34 +0700

    Update cfg.ara.callback.py

--- cfg.ara.callback.py
+++ cfg.ara.callback.py
@@ -1,1 +1,84 @@
+#
+
+#
+/usr/lib/python2.7/site-packages/ara
+/usr/local/lib/python2.7/dist-packages/ara
+$VIRTUAL_ENV/lib/python2.7/site-packages/ara
+
+
+#
+python -c "import os,ara; print(os.path.dirname(ara.__file__))"
+
+
+
+
+
+# Using ansible.cfg
+#------------------------------------------------------------------------------------------------
+
+export ara_location=$(python -c "import os,ara; print(os.path.dirname(ara.__file__))")
+
+cat > ansible.cfg <<EOF
+[defaults]
+# callback_plugins configuration is required for the ARA callback
+callback_plugins = $ara_location/plugins/callbacks
+
+# action_plugins and library configuration is required for the ara_record and ara_read modules
+action_plugins = $ara_location/plugins/actions
+library = $ara_location/plugins/modules
+EOF
+
+
+
+
+# Using environment variables
+#------------------------------------------------------------------------------------------------
+
+export ara_location=$(python -c "import os,ara; print(os.path.dirname(ara.__file__))")
+export ANSIBLE_CALLBACK_PLUGINS=$ara_location/plugins/callbacks
+export ANSIBLE_ACTION_PLUGINS=$ara_location/plugins/actions
+export ANSIBLE_LIBRARY=$ara_location/plugins/modules
+
+
+
+
+
+
+# ARA
+#------------------------------------------------------------------------------------------------
+
+# The order of priority is the following:
+-   Environment variables
+-   ./ansible.cfg (In the current working directory)
+-   ~/.ansible.cfg (In the home directory)
+-   /etc/ansible/ansible.cfg
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
