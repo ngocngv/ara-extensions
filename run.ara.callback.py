97988fee71b1121806c9652d189b12d432602fab
System <Mr.NN91@gmail.com>
2017-02-21 17:27:56 +0700

    Update run.ara.callback.py

--- run.ara.callback.py
+++ run.ara.callback.py
@@ -2,6 +2,45 @@
 
 
 # Embedded server
-#ara-manage runserver --help  
+# ara-manage runserver --help
+
+ara-manage runserver
+
+
+# database default
+export ARA_DATABASE="sqlite:///~/.ara/ansible.sqlite" 
+
+#
+export ARA_DATABASE="sqlite:////tmp/ara.sqlite"
+
+
+ara-manage runserver -h 0.0.0.0 -p 8080
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
