9baeda217c23491552aee28d41c6560f9b22c237
System <Mr.NN91@gmail.com>
2017-02-21 14:05:42 +0700

    Update install.ara.callback.py

--- install.ara.callback.py
+++ install.ara.callback.py
@@ -9,6 +9,23 @@
 
 
 
+# Required dependencies
+apt-get -y install gcc python-dev libffi-dev libssl-dev
+
+
+# Development or integration testing dependencies
+apt-get -y install python-pip libxml2-dev libxslt1-dev
+pip install tox
+
+
+
+# Installing ARA from trunk source
+pip install git+https://git.openstack.org/openstack/ara
+  
+# Installing ARA from latest release on PyPi
+pip install ara
+
+
 
 
 
